68aeb350cf579fefc1b81c900ed66387940b739c
genesiss <s21genesiss@gmail.com>
2026-01-14 23:16:34 +0700

    main: 004 commit. Add info.

--- Converter_python_exe/debug.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Запуск с отладкой...
-echo.
-python converter_app.py
-echo.
-echo Программа завершилась с кодом: %errorlevel%
-pause
--- Converter_python_exe/run_no_console.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start "" "%~dp0venv\Scripts\pythonw.exe" "%~dp0converter_app.py"
